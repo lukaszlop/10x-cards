585133f99bee58f021a79403fd4dc0dd7a9d9bce
Łukasz Łopieński <lukaszlop@gmail.com>
2025-06-11 17:49:36 +0200

    fix(astro-middleware): resolve import issues

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,8 +1,8 @@
-import { defineMiddleware, MiddlewareContext, MiddlewareNext } from "astro:middleware";
+import { defineMiddleware } from "astro:middleware";
 
 import { supabaseClient } from "../db/supabase.client";
 
-export const onRequest = defineMiddleware((context: MiddlewareContext, next: MiddlewareNext) => {
+export const onRequest = defineMiddleware((context, next) => {
   context.locals.supabase = supabaseClient;
   return next();
 });
